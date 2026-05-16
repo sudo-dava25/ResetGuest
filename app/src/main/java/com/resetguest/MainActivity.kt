5814b3c744d3d00a84668fad735ae2c01b7d706a
truncate -s 0 <davaanggasaputra25@gmail.com>
2026-05-16 11:58:10 +0700

    Update MainActivity.kt

--- app/src/main/java/com/resetguest/MainActivity.kt
+++ app/src/main/java/com/resetguest/MainActivity.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 
-class MainActivity : ComponentActivity() {
+val BgDeep = Color(0xFF080B10)
 
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val viewModel: MainViewModel = viewModel()
-            Surface(modifier = Modifier.fillMaxSize(), color = BgDeep) {
-                ResetGuestApp(viewModel = viewModel)
-            }
+            val vm: MainViewModel = viewModel()
+            ResetGuestApp(
+                viewModel = vm,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(BgDeep)
+            )
         }
     }
 }
